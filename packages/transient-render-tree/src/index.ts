cd3c8e24ebe111a35a805e3e4eea1e22e78f314c
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-16 07:57:56 -0300

    refactor(transient-render-tree): export serializable utils

--- packages/transient-render-tree/src/index.ts
+++ packages/transient-render-tree/src/index.ts
@@ -6,4 +6,13 @@ export type { TNode } from './tree/TNode';
 export type { TText } from './tree/TText';
 export type { TPhrasing } from './tree/TPhrasing';
 export type { TEmpty } from './tree/TEmpty';
+export type {
+  SerializableElement,
+  SerializableNode,
+  SerializableText
+} from './dom/to-serializable';
+export {
+  isSerializableElement,
+  isSerializableText
+} from './dom/to-serializable';
 export { assembleTTree as default } from './assemble';
