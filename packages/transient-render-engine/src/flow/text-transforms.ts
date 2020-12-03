6fad2154a56c231225e9f9152b36660ce034c59c
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-12-03 07:04:45 -0300

    fix(transient-render-engine): use unicode escape sequences inside regex literal
    Otherwise, some minifiers will replace those sequences with unicodes, which
    will result in a SyntaxError for chrome browsers.

--- packages/transient-render-engine/src/flow/text-transforms.ts
+++ packages/transient-render-engine/src/flow/text-transforms.ts
@@ -1,16 +1,10 @@
-const unicodeSpaceDiscarding =
-  '[\u2E80-\u2EFF\u2F00-\u2FDF\u2FF0-\u2FFF\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\u3130-\u318F\u3190-\u319F\u31C0-\u31EF\u31F0-\u31FF\u3300-\u33FF\u3400-\u4DBF\u4E00-\u9FFF\uA000-\uA48F\uA490-\uA4CF\uF900-\uFAFF\uFE10-\uFE1F\uFE30-\uFE4F\uFE50-\uFE6F\uFF00-\uFFEF]';
-
 // TODO, when React Native supports unicode regexes,
 // we should use `p{Zs}` instead of [ \t] to comply
 // with the CSS3 Text Module standard. https://www.w3.org/TR/css-text-3/#spaces
 const COLLAPSIBLE_AROUND_SEGMENT_BREAK_REGEX = /[ \t]*[\r\n\f]+[ \t]*/g;
 const CONSECUTIVE_SEGMENT_BREAKS_REGEX = /[\r\n\f]+/g;
 const ZERO_WIDTH_SPACES_AROUND_SEGMENT_BREAKS_REGEX = /(\u200b\n\u200b?|\u200b?\n\u200b)/g;
-const SPACE_DISCARDING_CHARSET_REGEX = new RegExp(
-  `(${unicodeSpaceDiscarding})\n(${unicodeSpaceDiscarding})`,
-  'g'
-);
+const SPACE_DISCARDING_CHARSET_REGEX = /([\u2E80-\u2EFF\u2F00-\u2FDF\u2FF0-\u2FFF\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\u3130-\u318F\u3190-\u319F\u31C0-\u31EF\u31F0-\u31FF\u3300-\u33FF\u3400-\u4DBF\u4E00-\u9FFF\uA000-\uA48F\uA490-\uA4CF\uF900-\uFAFF\uFE10-\uFE1F\uFE30-\uFE4F\uFE50-\uFE6F\uFF00-\uFFEF])\n([\u2E80-\u2EFF\u2F00-\u2FDF\u2FF0-\u2FFF\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\u3130-\u318F\u3190-\u319F\u31C0-\u31EF\u31F0-\u31FF\u3300-\u33FF\u3400-\u4DBF\u4E00-\u9FFF\uA000-\uA48F\uA490-\uA4CF\uF900-\uFAFF\uFE10-\uFE1F\uFE30-\uFE4F\uFE50-\uFE6F\uFF00-\uFFEF])/g;
 const SEGMENT_BREAK_REGEX = /\n/g;
 const SUBSEQUENT_WHITESPACE_REGEX = /[ \t]+/g;
 
