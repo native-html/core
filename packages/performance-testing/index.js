b7606de76354abd4fc0234c62b8ca7f04618999c
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-05-03 20:04:19 -0300

    chore: fix yarn perf script

--- packages/performance-testing/index.js
+++ packages/performance-testing/index.js
@@ -15,35 +15,41 @@ function makeBenchmark(name, options, target) {
     );
   }
 
-  suite
-    .add('Performance on a 65kb html snippet', () => {
-      translateOnlyTRE.buildTTree(rnImageDoc);
-    })
-    .on('error', function (e) {
-      console.error(e);
-      assert.strictEqual(
-        e === null,
-        true,
-        'TRenderEngine.buildTTree taised an error.'
-      );
-    })
-    .on('complete', function (event) {
-      const stats = event.target.stats;
-      const meanMs = stats.mean * 1000;
-      const varMs = stats.variance * 1000;
-      console.info('Mean is', meanMs, 'ms');
-      console.info('Variance is', varMs, 'ms');
-      assertExecLessThan(20, meanMs);
-    })
-    // run tests async
-    .run({ async: true });
+  return (
+    suite
+      .add('Performance on a 65kb html snippet', () => {
+        translateOnlyTRE.buildTTree(rnImageDoc);
+      })
+      .on('error', function (e) {
+        console.error(e);
+        assert.strictEqual(
+          e === null,
+          true,
+          'TRenderEngine.buildTTree taised an error.'
+        );
+      })
+      .on('complete', function (event) {
+        const stats = event.target.stats;
+        const meanMs = stats.mean * 1000;
+        const varMs = stats.variance * 1000;
+        console.info('Mean is', meanMs, 'ms');
+        console.info('Variance is', varMs, 'ms');
+        assertExecLessThan(meanMs);
+      })
+      // run tests async
+      .run({ async: true })
+  );
 }
 
-makeBenchmark(
-  'translate only',
-  {
-    dangerouslyDisableHoisting: true,
-    dangerouslyDisableWhitespaceCollapsing: true
-  },
-  20
-);
+async function run() {
+  await makeBenchmark(
+    'translate only',
+    {
+      dangerouslyDisableHoisting: true,
+      dangerouslyDisableWhitespaceCollapsing: true
+    },
+    20
+  );
+}
+
+return run();
