20d5cbccb47218465bfb6706894b83841e863b88
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2021-05-05 15:11:23 -0300

    ci: remove performance testing in tre workflow

--- packages/performance-testing/index.js
+++ packages/performance-testing/index.js
@@ -55,7 +55,7 @@ function makeBenchmark(name, options, target) {
 }
 
 async function run() {
-  const translateTarget = 20;
+  const translateTarget = 30;
   const { meanMs: translateMeanMs } = await makeBenchmark(
     'translate only',
     {
